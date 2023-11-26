a72b319bdfb6e063ed151c594cb5b5a91e9b6ce3
Yuanuanuan <asdll6618@gmail.com>
2023-11-26 12:11:35 +0800

    feat: progress

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,10 +3,9 @@ import Answer from "./Answer";
 import QUESTIONS from "../questions";
 
 export default function Question({ activeQuestionIndex, onSelectAnswer }) {
-
   return (
     <div id="question">
-      <QuestionTimer />
+      <QuestionTimer timeout={10000} onTimeout={() => onSelectAnswer(null)} />
       <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
       <Answer
         activeQuestionIndex={activeQuestionIndex}
--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,3 +1,17 @@
-export default function QuestionTimer() {
-  return <progress />;
+import { useState, useEffect } from "react";
+
+export default function QuestionTimer({ timeout, onTimeout }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    setTimeout(onTimeout, timeout);
+  }, []);
+
+  useEffect(() => {
+    setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 100);
+    }, 100);
+  }, []);
+
+  return <progress id="question-time" max={timeout} value={remainingTime} />;
 }
